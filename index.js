62f99bc1a3ff194da4f1d53381994b66b338fca4
t-wataru <tsujimoto.wataru@g.wakayama-u.jp>
2020-08-20 00:07:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 data.pages[0].lines.map(
 	line=>line
+		.replace(/\[(https:\/\/(?:.*?)(.jpg|.jpeg|.png|.gif))]/g, "![]($1)")
 		.replace(/\[(https:\/\/gyazo.com\/(?:.*?))]/g, "![]($1.png)")
 		.replace(/\[((?:.*?))\ (https?:\/\/[^\s]*)]/g, "[$1]($2)")
 		.replace(/\[(https?\/\/[^\s]*) ((?:.*?))\]/g, "[$2]($1)")
