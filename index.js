c0cda06aaa477600af15af76020d4cf710f5eddc
t-wataru <tsujimoto.wataru@g.wakayama-u.jp>
2020-08-19 23:50:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+data.pages[0].lines.map(
+	line=>line
+		.replace(/\[(https:\/\/gyazo.com\/(?:.*?))]/g, "![]($1.png)")
+		.replace(/\[((?:.*?))\ (https?:\/\/[^\s]*)]/g, "[$1]($2)")
+		.replace(/\[(https?\/\/[^\s]*) ((?:.*?))\]/g, "[$2]($1)")
+)
