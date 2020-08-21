d273b3648ed33cd19d65bbfd098d120147ce7448
t-wataru <tsujimoto.wataru@g.wakayama-u.jp>
2020-08-22 00:32:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 data.pages.map(
 	page=>page.lines.map(
-		line=>line
-			.replace(/\[(https:\/\/(?:.*?)(.jpg|.jpeg|.png|.gif))]/g, "![]($1)")
-			.replace(/\[(https:\/\/gyazo.com\/(?:.*?))]/g, "![]($1.png)")
-			.replace(/\[((?:.*?))\ (https?:\/\/[^\s]*)]/g, "[$1]($2)")
-			.replace(/\[(https?\/\/[^\s]*) ((?:.*?))\]/g, "[$2]($1)")
-	)
+		line=>{
+			let newLine = line.replace(/\[(https:\/\/(?:.*?)(.jpg|.jpeg|.png|.gif))]/g, "![]($1)")
+				.replace(/\[(https:\/\/gyazo.com\/(?:.*?))]/g, "![]($1.png)")      
+				.replace(/\[((?:.*?))\s+(https?:\/\/[^\s]*)]/g, "[$1]($2)")
+				.replace(/\[(https?:\/\/[^\s]*)\s+((?:.*?))\]/g, "[$2]($1)")
+				.replace(/\[(https?:\/\/[^\s]*)\]/g, "$1");
+      			if(newLine == line) {newLine = line.replace(/(\[(?:.*?)\])/g, "[$1]")}
+			if(newLine == line) {newLine = line.replace(/(\[(\*+) ((?:.*?))\])/g, (match, p1, p2)=>`<font size="+${p1.length+1}">${p2}</font>`)}
+      			return newLine;
+    		}  
+	).join("\n")
 )
